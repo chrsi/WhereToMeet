16751b02d647ae2b82524409e86c936c54a1e9c5
Christian Siber <christian.siber@dataformers.at>
2018-11-04 16:17:23 +0100

    add existing meeting navigation

--- WhereToMeet/Controllers/HomeController.cs
+++ WhereToMeet/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WhereToMeet.Controllers
@@ -11,6 +8,17 @@ namespace WhereToMeet.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult GoToMeeting([FromForm] Guid meetingId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction("Index", "Meeting", new { meetingId = meetingId });
         }
     }
 }
