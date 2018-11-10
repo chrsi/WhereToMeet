1e11fe8249daaab7575bffb48ab6ab493c4bb186
Christian Siber <siber.christian@gmail.com>
2018-11-10 15:51:48 +0100

    Upgrade .net core 2.0 -> 2.1 and force ssl usage

--- WhereToMeet/Startup.cs
+++ WhereToMeet/Startup.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,6 +40,7 @@ namespace WhereToMeet
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
             app.UseMvcWithDefaultRoute();
         }
     }
